a1c7f87989ac9bd57075a62bfe1f34ba98b4600c
davidmoore1 <david_moore1@sil.org>
2024-07-11 09:15:48 -0400

    Change script to work on build machine

--- resources/build/xcode-wrap.sh
+++ resources/build/xcode-wrap.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 echo "wrap script for arch $(arch)"
-if [[ $(arch) == i386 ]] && [[ -f /usr/local/bin/bash ]]; then
+if [[ -f /usr/local/bin/bash ]]; then
   /usr/local/bin/bash -l "$@" || exit $?
-elif [[ $(arch) == arm64 ]] && [[ -f /opt/homebrew/bin/bash ]]; then
+elif [[ -f /opt/homebrew/bin/bash ]]; then
   /opt/homebrew/bin/bash -l "$@" || exit $?
 else
   >&2 echo "Could not start build due to missing homebrew bash"
